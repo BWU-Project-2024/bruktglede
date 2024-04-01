55f95c28f3a58e780fa9fc00275f075622539787
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-01 16:35:13 +0200

    added login link to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,23 @@
+import Link from "next/link";
+
 export const Footer = ({ title, description }) => {
     return (
         <footer className="text-center w-full py-4 bg-forestgreen-default m-0">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <Link href="" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
 
                         <span className="self-center text-5xl font-jomhuria whitespace-nowrap text-ivory-default tracking-wide px-6 md:px-20 g:px-40">Bruktglede</span>
-                    </a>
+                    </Link>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-ivory-default sm:mb-0 px-6 md:px-20">
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Om oss</a>
+                            <Link href="#" className="hover:underline me-4 md:me-6">Om oss</Link>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                            <Link href="#" className="hover:underline me-4 md:me-6">Privacy Policy</Link>
                         </li>
-
                         <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <Link href="/CMS/login" className="hover:underline">Login</Link>
                         </li>
                     </ul>
                 </div>
