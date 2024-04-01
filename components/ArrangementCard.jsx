9a719af3fa8a80baf6be43f977502563d6dcf541
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-01 13:28:02 +0200

    arrangement card

---
+++ components/ArrangementCard.jsx
@@ -0,0 +1,35 @@
+import { StoreTag } from "./StoreTag";
+import Image from "next/image";
+import test from "../app/assets/img/test.jpg";
+
+export const ArrangementCard = ({ type, title, description, store }) => {
+    return (
+        <main className="relative h-auto w-3/4 md:w-4/12 rounded-xl shadow-md py-5 px-4 mb-14 md:mx-2 ml-2">
+            <div className="mb-3 relative">
+                <Image src={test} alt="Test Image" width={500} height={300} />
+                <div
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white flex flex-col items-center justify-center"
+                    style={{
+                        width: "130px",
+                        height: "130px",
+                        borderRadius: "50%",
+                    }}
+                >
+                    <span className="text-7xl/9 font-bold font-jomhuria">
+                        24
+                    </span>
+                    <span className="text-3xl max-h-8">JUN</span>
+                </div>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+                <p className="pb-3 text-sm md:text-base flex-grow font-semibold">
+                    {type}
+                </p>
+                <StoreTag>{store}</StoreTag>{" "}
+                {/* Pass the store name as a prop */}
+            </div>
+            <h3 className="font-bold text-base pb-3 underline">{title}</h3>
+            <p>{description}</p>
+        </main>
+    );
+};
--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,19 +1,37 @@
-import { Header } from "../Header"
-import { StoreTag } from "../StoreTag"
-import { CategoryTag } from "../CategoryTag"
+import { Header } from "../Header";
+import { StoreTag } from "../StoreTag";
+import { CategoryTag } from "../CategoryTag";
+import { ArrangementCard } from "../ArrangementCard";
+import { PiHouseLineLight, PiMapPinLineLight, PiTimer } from "react-icons/pi";
 
 export const HomePage = () => {
     return (
-      <div>
-         <Header title="Homepage" description="Beskrivende tekst"></Header>
-            
-        <main>
-           
-          <StoreTag title="Fretex"></StoreTag>
-          <CategoryTag bgColor="ivory-default" title="Klær"></CategoryTag>
-          
-           
-        </main>
+        <div>
+            <Header title="Homepage" description="Beskrivende tekst"></Header>
+
+            <main>
+                <StoreTag title="Fretex"></StoreTag>
+                <CategoryTag bgColor="ivory-default" title="Klær"></CategoryTag>
+                <ArrangementCard
+                    type="Arrangement"
+                    store="Fretex"
+                    title="Gratis middag for studenter"
+                    description={
+                        <>
+                            <div className="flex items-center gap-3">
+                                {" "}
+                                <PiHouseLineLight /> Fretex Gjøvik{" "}
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <PiMapPinLineLight /> Lalala vegen 30
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <PiTimer /> 16.00 - 20.00{" "}
+                            </div>
+                        </>
+                    }
+                />
+            </main>
         </div>
-    )
-}
+    );
+};
