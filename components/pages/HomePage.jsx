5f22129c0c81b86ef6ed10b8cabf0af040b49927
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-02 00:14:54 +0200

    updated article card

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -3,16 +3,15 @@ import { StoreTag } from "../StoreTag";
 import { CategoryTag } from "../CategoryTag";
 import { Footer } from "../Footer";
 import { LandingHeader } from "../LandingHeader";
-import { EventCard } from "../EventCard";
-import { LandingHeader } from "../LandingHeader"
-import { Button } from "../Button"
+import { ArticleCard } from "../ArticleCard";
+import { Button } from "../Button";
 
 export const HomePage = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <LandingHeader />
             <main className="flex-1">
-                <EventCard
+                <ArticleCard
                     type="Artikkel"
                     store="Fretex"
                     title="Vi har pusset opp butikken!"
