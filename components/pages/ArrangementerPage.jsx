7192181c52e1dd43d490ced891fb64c38580d517
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-01 13:46:42 +0200

    changed to right page

--- components/pages/ArrangementerPage.jsx
+++ components/pages/ArrangementerPage.jsx
@@ -1,10 +1,27 @@
-
+import { ArrangementCard } from "../ArrangementCard";
+import { PiHouseLineLight, PiMapPinLineLight, PiTimer } from "react-icons/pi";
 export const ArrangementerPage = () => {
     return (
         <main>
-            <h1>
-                Arrangementer page
-            </h1>
+            <ArrangementCard
+                type="Arrangement"
+                store="Fretex"
+                title="Gratis middag for studenter"
+                description={
+                    <>
+                        <div className="flex items-center gap-3">
+                            {" "}
+                            <PiHouseLineLight /> Fretex Gjøvik{" "}
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <PiMapPinLineLight /> Lalala vegen 30
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <PiTimer /> 16.00 - 20.00{" "}
+                        </div>
+                    </>
+                }
+            />
         </main>
-    )
-}
+    );
+};
--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,8 +1,6 @@
 import { Header } from "../Header";
 import { StoreTag } from "../StoreTag";
 import { CategoryTag } from "../CategoryTag";
-import { ArrangementCard } from "../ArrangementCard";
-import { PiHouseLineLight, PiMapPinLineLight, PiTimer } from "react-icons/pi";
 
 export const HomePage = () => {
     return (
@@ -12,25 +10,6 @@ export const HomePage = () => {
             <main>
                 <StoreTag title="Fretex"></StoreTag>
                 <CategoryTag bgColor="ivory-default" title="Klær"></CategoryTag>
-                <ArrangementCard
-                    type="Arrangement"
-                    store="Fretex"
-                    title="Gratis middag for studenter"
-                    description={
-                        <>
-                            <div className="flex items-center gap-3">
-                                {" "}
-                                <PiHouseLineLight /> Fretex Gjøvik{" "}
-                            </div>
-                            <div className="flex items-center gap-3">
-                                <PiMapPinLineLight /> Lalala vegen 30
-                            </div>
-                            <div className="flex items-center gap-3">
-                                <PiTimer /> 16.00 - 20.00{" "}
-                            </div>
-                        </>
-                    }
-                />
             </main>
         </div>
     );
