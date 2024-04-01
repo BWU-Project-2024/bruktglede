63f63c937413936fe4e47be719aefdf91db89c16
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-01 14:26:11 +0200

    event card

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,22 +1,23 @@
-import { Header } from "../Header"
-import { StoreTag } from "../StoreTag"
-import { CategoryTag } from "../CategoryTag"
-import { Footer } from "../Footer"
-import { LandingHeader } from "../LandingHeader"
-
+import { Header } from "../Header";
+import { StoreTag } from "../StoreTag";
+import { CategoryTag } from "../CategoryTag";
+import { Footer } from "../Footer";
+import { LandingHeader } from "../LandingHeader";
+import { EventCard } from "../EventCard";
 
 export const HomePage = () => {
     return (
-      <div className="flex flex-col min-h-screen">
-         <LandingHeader />
-            
-        <main className="flex-1">
-           
-         
-           
-        </main>
-        <Footer />
+        <div className="flex flex-col min-h-screen">
+            <LandingHeader />
+            <main className="flex-1">
+                <EventCard
+                    type="Artikkel"
+                    store="Fretex"
+                    title="Vi har pusset opp butikken!"
+                    description="De siste månedene har vi jobbet med å gjøre butikken fin. Dette har vært..."
+                />
+            </main>
+            <Footer />
         </div>
-    )
-}
-
+    );
+};
