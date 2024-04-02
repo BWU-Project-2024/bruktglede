b15fb05e877dfb5c0b1c4ff2a30440f789a4b47c
lexavan <alexaev@stud.ntnu.no>
2024-04-02 15:55:52 +0200

    om oss + diverse

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import { CategoryTag } from "../CategoryTag";
 import { Footer } from "../Footer";
 import { LandingHeader } from "../LandingHeader";
 import { EventCard } from "../EventCard";
-import { LandingHeader } from "../LandingHeader"
 import { Button } from "../Button"
 
 export const HomePage = () => {
@@ -19,7 +18,7 @@ export const HomePage = () => {
                     description="De siste månedene har vi jobbet med å gjøre butikken fin. Dette har vært..."
                 />
             </main>
-            <Footer />
+          
             <main className="flex-1">
                 <Button title="Se alle arrangementer" link="/" />
             </main>
