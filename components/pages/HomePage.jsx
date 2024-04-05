8a23653e5c56add0de0521cbe74eb759eb12742f
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-05 16:37:22 +0200

    ukens høydepunkt

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import { LandingHeader } from "../LandingHeader";
 import { Button } from "../Button";
+import tekopp from "@/public/tekopp.jpg";
+import { UkensHoydepunkt } from "../UkensHoydepunkt";
 
 export const HomePage = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <LandingHeader />
+
             <main className="flex-1">
                 <Button title="Se alle arrangementer" link="/" />
             </main>
