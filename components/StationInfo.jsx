dc7a3e3c90c975bcb9411b0053a32a522f3a59d1
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-07 18:44:17 +0200

    fixed the data fetching

--- components/StationInfo.jsx
+++ components/StationInfo.jsx
@@ -1,89 +1,41 @@
-"use client";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import { CategoryTag } from "./CategoryTag";
 import {
-    fetchStoreInfo,
-    fetchStationsData,
-    fetchTagsData,
+    fetchAllStationsSortedByStore,
 } from "@/lib/supabase/actions";
 
-export const StationInfo = () => {
-    const [storesWithStations, setStoresWithStations] = useState([]);
+export const StationInfo = async () => {
+    const storeInfo = await fetchAllStationsSortedByStore();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const storeinfo = await fetchStoreInfo();
-                const stationsData = await Promise.all(
-                    storeinfo.map(async (store) => {
-                        const stationData = await fetchStationsData(store.id);
-                        const tagsData = await Promise.all(
-                            stationData.map(
-                                async (station) =>
-                                    await fetchTagsData(station.id)
-                            )
-                        );
-                        const stationsWithTag = stationData.map(
-                            (station, index) => ({
-                                ...station,
-                                tags: tagsData[index] || [], // Using tag names or an empty array if no tags found
-                            })
-                        );
-                        return stationsWithTag;
-                    })
-                );
-
-                const storesWithStations = storeinfo
-                    .map((store, index) => ({
-                        ...store,
-                        stations: stationsData[index],
-                    }))
-                    .filter((store) => store.stations.length > 0);
-
-                setStoresWithStations(storesWithStations);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        fetchData();
-    }, []);
-
-
-    
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-10">
-            {storesWithStations.map((store) => (
-                <div key={store.id}>
+            {storeInfo.map((storeData) => (
+                <div key={storeData.store.id}>
                     <h2 className="text-xl lg:text-2xl font-semibold font-opensans mb-1 mt-10">
-                        {store.name}
+                        {storeData.store.name}
                     </h2>
-                    <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-                    <div>
-                        {store.stations.map((station) => (
-                            <div key={station.id}>
-                                <h3 className="font-opensans text-base md:text-lg font-semibold text-color-jet mb-3 -mt-1">
-                                    {station.title}
-                                </h3>
-                                <p className="text-base md:text-lg underline text-color-jet mb-2  font-opensans lg:text-lg">
-                                    {station.address}
-                                </p>
-                                <div>
-                                    <div className="mt-3 mb-10">
-                                        <h4 className="uppercase font-opensans text-sm font-medium md:mb-2">
-                                            Dette tas imot
-                                        </h4>
-                                        {station.tags.map((tagName) => (
-                                            <CategoryTag
-                                                key={tagName}
-                                                title={tagName}
-                                            />
-                                        ))}
-                                    </div>
+                    <hr className="h-px my-3 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                    {storeData.stations.map((station) => (
+                        <div key={station.id} className="flex flex-col gap-3">
+                            <h3 className="font-opensans text-base md:text-lg font-semibold text-color-jet">
+                                {station.title}
+                            </h3>
+                            <p className="text-base md:text-lg underline text-color-jet font-opensans lg:text-lg">
+                                {station.address}
+                            </p>
+                            <div>
+                                <div className="mb-10">
+                                    <h4 className="uppercase font-opensans text-sm font-medium md:mb-3">
+                                        Dette tas imot
+                                    </h4>
+                                    {station.tags.map((tag) => (
+                                        <CategoryTag key={tag} title={tag} />
+                                    ))}
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
+
                 </div>
             ))}
         </div>
