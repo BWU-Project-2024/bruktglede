f4f8a4db2953dbbec11e242d892fdcab30221f68
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-09 15:05:28 +0200

    updated store info bar

--- components/StoreInfoBar.jsx
+++ components/StoreInfoBar.jsx
@@ -5,36 +5,38 @@ export const StoreInfoBar = ({ time, address, phone, mail, categories }) => {
     const iconStyle = { fontSize: "1.1em" };
 
     return (
-        <div className="w-full py-4 bg-forestgreen-default mb-5">
-            <div className="p-5">
-                <div className="flex flex-col gap-2">
-                    <div className="flex items-center gap-8 text-ivory-default">
-                        <FiClock style={iconStyle} />
-                        <p>{time}</p>
-                    </div>
-                    <div className="flex items-center gap-8 text-ivory-default">
-                        <FiMapPin style={iconStyle} />
-                        <p>{address}</p>
-                    </div>
-                    <div className="flex items-center gap-8 text-ivory-default">
-                        <FiPhone style={iconStyle} />
-                        <p>{phone}</p>
-                    </div>
-                    <div className="flex items-center gap-8 text-ivory-default">
-                        <FiMail style={iconStyle} />
-                        <p>{mail}</p>
+        <div className="w-full py-4 bg-forestgreen-default mb-5 lg:pb-16 md:pb-16">
+            <div className="p-5 flex flex-col lg:flex-row md:flex-row justify-center lg:gap-56 md:gap-56">
+                <div>
+                    <div className="flex flex-col gap-2 pl-2">
+                        <div className="flex items-center gap-8 text-ivory-default">
+                            <FiClock style={iconStyle} />
+                            <p>{time}</p>
+                        </div>
+                        <div className="flex items-center gap-8 text-ivory-default">
+                            <FiMapPin style={iconStyle} />
+                            <p>{address}</p>
+                        </div>
+                        <div className="flex items-center gap-8 text-ivory-default">
+                            <FiPhone style={iconStyle} />
+                            <p>{phone}</p>
+                        </div>
+                        <div className="flex items-center gap-8 text-ivory-default">
+                            <FiMail style={iconStyle} />
+                            <p>{mail}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="pt-5">
-                <span className="uppercase text-ivory-default p-5">
-                    Overordnet kategorier
-                </span>
-                <div className="flex flex-wrap pl-3 pt-3">
-                    {categories &&
-                        categories.map((tagName, index) => (
-                            <CategoryTag key={index} title={tagName} />
-                        ))}
+                <div className="pt-5 lg:pt-0 md:pt-0">
+                    <span className="uppercase text-ivory-default pl-2">
+                        Overordnet kategorier
+                    </span>
+                    <div className="flex flex-wrap pt-3">
+                        {categories &&
+                            categories.map((tagName, index) => (
+                                <CategoryTag key={index} title={tagName} />
+                            ))}
+                    </div>
                 </div>
             </div>
         </div>
