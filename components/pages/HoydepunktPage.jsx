4eab9fb20996d9ca1605d6ad5e17b023afed507d
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-11 12:28:26 +0200

    ukens høydepunkter page

--- components/pages/HoydepunktPage.jsx
+++ components/pages/HoydepunktPage.jsx
@@ -1,12 +1,32 @@
-import { Header } from "../Header"
+import { Header } from "../Header";
+import tekopp from "@/public/tekopp.jpg";
+import { UkensHoydepunkt } from "../UkensHoydepunkt";
 
 export const HoydepunktPage = () => {
     return (
         <div className="flex flex-col min-h-screen">
-            <Header title="Ukens høydepunkt" description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke." />
-            <main className="flex-1">
+            <Header
+                title="Ukens høydepunkt"
+                description="Hver uke plukker butikkene ut hvert sitt spesielle produkt som ukens høydepunkt."
+            />
 
-            </main>
+            <h3 className="font-semibold pl-4 py-5">
+                Høydepunkt hos Fretex uke 25
+            </h3>
+
+            <UkensHoydepunkt
+                image={tekopp}
+                text="Antikk kopp"
+                store="Fretex"
+                description="Denne uken er ukens høydepunkt en antikk kopp, som skinner i sin eldgamle prakt..."
+            />
+
+            <h3 className="font-semibold pl-4">Hold deg oppdatert</h3>
+            <p className="pl-4 pt-3 w-11/12">
+                Ønsker du å bli opplyst om ukens highlight? Legg igjen epost
+                adressen din for å holde deg oppdatert!{" "}
+            </p>
+            <main className="flex-1"></main>
         </div>
-    )
-}
+    );
+};
