ded202bf53bd1d7b9e7bae57fa9abb0ec6e2351b
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-23 19:21:12 +0200

    fått til at man ser ulike ting basert på rolle ish

---
+++ app/CMS/admin/page.js
@@ -0,0 +1,7 @@
+export default async function admin() {
+    return (
+        <div className="sm:hidden flex flex-col min-h-[90vh] w-full">
+            <p>hello admin</p>
+        </div>
+    );
+}
--- app/CMS/layout.jsx
+++ app/CMS/layout.jsx
@@ -4,8 +4,9 @@ import { createClient } from '@/lib/supabase/supabaseServer';
 import { readUserSession } from '@/lib/supabase/actionsAuth';
 
 export default async function Layout({ children }) {
-
-    const { data: { session } } = await readUserSession();
+    const data = await readUserSession();
+    const session = data?.session;
+    const role = data?.roleData[0]?.role;
 
     if (!session) {
         return redirect('/login');
@@ -21,15 +22,26 @@ export default async function Layout({ children }) {
 
     return (
         <>
-            {/* desktop layout */}
-            <section className="hidden sm:flex sm:flex-row">
-                <CMSNavbar signOut={signOut} />
-                {children}
-            </section>
-            {/* mobile desktop */}
-            <section className="sm:hidden flex flex-col w-full">
-                {children}
-            </section>
+            {session && role === 'superuser' && (
+                <>
+                    {/* desktop layout */}
+                    <section className="hidden sm:flex sm:flex-row">
+                        <CMSNavbar signOut={signOut} />
+                        {children}
+                    </section>
+                    {/* mobile desktop */}
+                    <section className="sm:hidden flex flex-col w-full">
+                        {children}
+                    </section>
+                </>
+            )}
+
+            {session && role === 'admin' && (
+                <div>
+                    <p>hello admin</p>
+                    {children}
+                </div>
+            )}
         </>
     );
 }
