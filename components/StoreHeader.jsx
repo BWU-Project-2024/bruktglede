852f279a293014845af09c034e43aaf023eeee96
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-24 15:26:08 +0200

    fixed the store header

--- components/StoreHeader.jsx
+++ components/StoreHeader.jsx
@@ -2,39 +2,41 @@
 import Image from "next/image";
 import { useState } from "react";
 
-export const StoreHeader = ({ image, store, description, vision, text }) => {
-    const [showVisionText, setShowVisionText] = useState(false);
+export const StoreHeader = ({ image, store, description, vision }) => {
+    const [isVisionVisible, setIsVisionVisible] = useState(false);
 
-    const toggleVisionText = () => {
-        setShowVisionText(!showVisionText);
+    const toggleVision = () => {
+        setIsVisionVisible(!isVisionVisible);
     };
-
-    const greenBackgroundHeight = showVisionText ? "h-auto" : "h-1/3";
 
     return (
         <div className="relative">
-            <div className="relative">
-                <Image src={image} alt="Image of the store" />
-            </div>
+            <Image src={image} alt="Image of the store" layout="responsive" />
             <div
-                className={`inset-x-0 bg-ivory-default bg-opacity-90 text-center px-7 py-3 lg:absolute lg:top-auto lg:bottom-0 lg:left-0 lg:right-0 lg:${greenBackgroundHeight} lg:px-40 lg:py-8`}
+                className={`relative inset-x-0 text-center px-7 py-3 bg-ivory-default ${isVisionVisible ? "bg-opacity-90" : "bg-opacity-70"} transition-opacity duration-300 `}
             >
-                <div className="absolute lg:bottom-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[110px] h-[110px] aspect-square rounded-full flex justify-center items-center font-jomhuria text-5xl">
+                <div className="absolute lg:-top-44 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[110px] lg:w-[150px] h-[110px] lg:h-[150px] aspect-square rounded-full flex justify-center items-center font-jomhuria text-5xl">
                     {store}
                 </div>
-                <p className="lg:max-w-2xl mx-auto lg:pt-3 pt-16">
-                    {description}
-                </p>
-                {showVisionText && (
-                    <p className="lg:max-w-4xl mx-auto lg:pt-3 pt-16">{text}</p>
+                <p className="lg:max-w-2xl mx-auto pt-16">{description}</p>
+                {isVisionVisible && (
+                    <p className="text-center mt-5">
+                        Visjonen til Fretex er å skape en bærekraftig og
+                        inkluderende samfunn der ingen ting går til spille.
+                        Fretex har en visjon om å redusere avfall og fremme
+                        miljøbevissthet ved å gi nytt liv til brukte varer.
+                        Våres mål er ikke bare å tilby rimelige produkter til
+                        kunder, men også å støtte ulike veldedige formål og
+                        sosiale initiativer.
+                    </p>
                 )}
-                <div className="flex justify-center pt-5">
-                    <div
+                <div className="flex justify-center pt-5 pb-3">
+                    <button
+                        onClick={toggleVision}
                         className="w-48 bg-ivory-lighter text-forestgreen-default rounded py-1 font-medium drop-shadow hover:bg-forestgreen-default hover:text-ivory-default transition duration-200 text-center"
-                        onClick={toggleVisionText}
                     >
                         {vision}
-                    </div>
+                    </button>
                 </div>
             </div>
         </div>
--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -2,12 +2,27 @@ import { LandingHeader } from "../LandingHeader";
 import { Button } from "../Button";
 import tekopp from "@/public/tekopp.jpg";
 import { UkensHoydepunkt } from "../UkensHoydepunkt";
+import { StoreHeader } from "../StoreHeader";
+import test from "@/public/test.jpg";
 
 export const HomePage = async () => {
     return (
         <div className="flex flex-col min-h-screen">
             <LandingHeader />
 
+            <StoreHeader
+                image={test}
+                store="Fretex"
+                description="Butikken er en av de eldste aktørene i byen. Her finner du alt av klær og tilbehør, inkludert andre ting som servise og sysaker. Ta turen innom, så skal vi hjelpe deg."
+                vision="Fretex visjon"
+                text="Visjonen til Fretex er å skape en bærekraftig og
+                inkluderende samfunn der ingen ting går til spille.
+                Fretex har en visjon om å redusere avfall og fremme
+                miljøbevissthet ved å gi nytt liv til brukte varer.
+                Våres mål er ikke bare å tilby rimelige produkter til
+                kunder, men også å støtte ulike veldedige formål og
+                sosiale initiativer."
+            />
 
             <UkensHoydepunkt
                 image={tekopp}
