ad31b2e13ad1513c1cd3c551b1beedd8626fecd4
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-24 15:31:01 +0200

    changed it to right page

--- components/pages/ButikkerPage.jsx
+++ components/pages/ButikkerPage.jsx
@@ -1,12 +1,26 @@
-
-import { getStores } from "@/app/butikker/actions"
-import { Header } from "../Header"
+import { getStores } from "@/app/butikker/actions";
+import { Header } from "../Header";
+import { StoreHeader } from "../StoreHeader";
+import test from "@/public/test.jpg";
 
 export async function ButikkerPage() {
     const { data: stores } = await getStores();
     return (
         <div className="flex flex-col min-h-screen">
-            <Header title="Alle butikker" description="Her finner du en oversikt over alle butikkene på plattformen" />
+            <StoreHeader
+                image={test}
+                store="Fretex"
+                description="Butikken er en av de eldste aktørene i byen. Her finner du alt av klær og tilbehør, inkludert andre ting som servise og sysaker. Ta turen innom, så skal vi hjelpe deg."
+                vision="Fretex visjon"
+                text="Visjonen til Fretex er å skape en bærekraftig og
+                inkluderende samfunn der ingen ting går til spille.
+                Fretex har en visjon om å redusere avfall og fremme
+                miljøbevissthet ved å gi nytt liv til brukte varer.
+                Våres mål er ikke bare å tilby rimelige produkter til
+                kunder, men også å støtte ulike veldedige formål og
+                sosiale initiativer."
+            />
+            {/*  <Header title="Alle butikker" description="Her finner du en oversikt over alle butikkene på plattformen" /> */}
             <main>
                 <h1>Butikker page</h1>
                 <ul>
@@ -16,5 +30,5 @@ export async function ButikkerPage() {
                 </ul>
             </main>
         </div>
-    )
+    );
 }
