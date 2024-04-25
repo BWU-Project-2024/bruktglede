89bee3cac012559d39fc4a53b2a93baf6e5801c9
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-25 14:53:35 +0200

    event actions public

--- components/ArrangementCard.jsx
+++ components/ArrangementCard.jsx
@@ -1,58 +1,83 @@
-import { StoreTag } from "./StoreTag";
-import Link from "next/link";
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { FiHome, FiMapPin, FiClock } from "react-icons/fi";
+import { StoreTag } from "./StoreTag";
+import { getEvents } from "@/lib/supabase/actionsPublic";
 
-export const ArrangementCard = ({ image, date, month, type, title, store, address, time }) => {
-    const iconStyle = { fontSize: "1.1em" }
+export const ArrangementCard = () => {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const fetchedEvents = await getEvents();
+                setEvents(fetchedEvents);
+            } catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
 
     return (
-        <div className="shadow-md rounded-xl w-[80%] md:w-[20%]">
-            <div className="h-auto w-100 relative flex justify-center items-center">
-                <Image
-                    src={image}
-                    alt="Arrangement cover bilde"
-                    width={500}
-                    height={200}
-                    className="rounded-tr-lg rounded-tl-lg"
+        <>
+            {events.map((event, index) => (
+                <div
+                    key={index}
+                    className="shadow-md rounded-xl w-[80%] md:w-[20%] mb-5"
                 >
-                </Image>
-                <div className="absolute w-[110px] aspect-square rounded-full bg-white flex justify-center items-center flex-col">
-                    <span className="text-6xl/6 font-bold font-jomhuria pt-2.5">
-                        {date}
-                    </span>
-                    <span className="text-2xl max-h-8">
-                        {month}
-                    </span>
-                </div>
-            </div>
+                    <div className="h-auto w-100 relative flex justify-center items-center">
+                        <Image
+                            src={event.img}
+                            alt="Arrangement cover image"
+                            width={500}
+                            height={200}
+                            className="rounded-tr-lg rounded-tl-lg"
+                        />
+                        <div className="absolute w-[110px] aspect-square rounded-full bg-white flex justify-center items-center flex-col">
+                            <span className="text-5xl/6 font-bold font-jomhuria pt-2.5">
+                                {event.store_name}
+                            </span>
+                            <span className="text-2xl max-h-8">
+                                {event.month}
+                            </span>
+                        </div>
+                    </div>
 
-            <div className="p-5">
-                <div className="flex flex-row items-center justify-center">
-                    <p className="text-sm md:text-md flex-grow font-semibold uppercase">
-                        {type}
-                    </p>
-                    <StoreTag>{store}</StoreTag>
+                    <div className="p-5">
+                        <div className="flex flex-row items-center justify-center">
+                            <p className="text-sm md:text-md flex-grow font-semibold uppercase">
+                                {event.postTypeName}
+                            </p>
+                            <StoreTag>{event.store_name}</StoreTag>
+                        </div>
+                        <h4 className="text-lg font-medium py-4 underline">
+                            <Link href={`/events/${event.id}`}>
+                                {event.title}
+                            </Link>
+                        </h4>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-center gap-2">
+                                <FiHome style={{ fontSize: "1.1em" }} />
+                                <p>{event.store_name}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <FiMapPin style={{ fontSize: "1.1em" }} />
+                                <p>{event.address}</p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <FiClock style={{ fontSize: "1.1em" }} />
+                                <p>
+                                    {event.startTime} - {event.endTime}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <h4 className="text-lg font-medium py-4 underline">
-                    <Link href="/">{title}</Link>
-                </h4>
-                <div className="flex flex-col gap-2">
-                    <div className="flex items-center gap-2">
-                        <FiHome style={iconStyle} />
-                        <p>{store}</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <FiMapPin style={iconStyle} />
-                        <p>{address}</p>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <FiClock style={iconStyle} />
-                        <p>{time}</p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+            ))}
+        </>
     );
 };
--- components/pages/ArrangementerPage.jsx
+++ components/pages/ArrangementerPage.jsx
@@ -1,6 +1,5 @@
 import { Header } from "../Header";
 import { ArrangementCard } from "../ArrangementCard";
-import test from "@/public/test.jpg";
 
 export const ArrangementerPage = () => {
     return (
@@ -9,18 +8,8 @@ export const ArrangementerPage = () => {
                 title="Arrangementer"
                 description="Her finner du en oversikt over alle kommende arrangementer"
             />
-            <main className="flex-1">
-                <ArrangementCard
-                    image={test}
-                    type="Arrangement"
-                    date="24"
-                    month="JUN"
-                    store="Fretex"
-                    title="Gratis middag for studenter"
-                    address="Gjøvikvegen 22, 2815 Gjøvik"
-                    time="10:00 - 12:00"
-                />
-            </main>
+            <ArrangementCard />
+            <main className="flex-1"></main>
         </div>
     );
 };
