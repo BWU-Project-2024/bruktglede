0dff3fe75438b9e6f8aa03cef4e2b214c75cdfe5
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-25 15:04:44 +0200

    fixed an issue

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import { LandingHeader } from "../LandingHeader";
 import { Button } from "../Button";
 import tekopp from "@/public/tekopp.jpg";
 import { UkensHoydepunkt } from "../UkensHoydepunkt";
-
 import { BliFrivilligCard } from "../BliFrivilligCard";
 
 export const HomePage = async () => {
@@ -10,25 +9,16 @@ export const HomePage = async () => {
         <div className="flex flex-col min-h-screen">
             <LandingHeader />
 
-
             <main className="">
                 <div className="flex-1  lg:-mt-10 p-2 px-6 md:px-20 lg:px-40 lg:pt-20">
-
-            <UkensHoydepunkt
-                image={tekopp}
-                text="Antikk kopp"
-                store="Fretex"
-                description="Denne uken er ukens høydepunkt en antikk kopp, som skinner i sin eldgamle prakt..."
-            />
-
-
-            <div></div>
-
-            <main className="flex-1">
-
+                    <UkensHoydepunkt
+                        image={tekopp}
+                        text="Antikk kopp"
+                        store="Fretex"
+                        description="Denne uken er ukens høydepunkt en antikk kopp, som skinner i sin eldgamle prakt..."
+                    />
                 </div>
-         <BliFrivilligCard />
-
+                <BliFrivilligCard />
                 <Button title="Se alle arrangementer" link="/" />
             </main>
         </div>
