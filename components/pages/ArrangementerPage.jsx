ffd15f7895e14b6fc04cec0d0a14a97293fbb623
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-25 16:19:59 +0200

    fikset arrangment data

--- components/pages/ArrangementerPage.jsx
+++ components/pages/ArrangementerPage.jsx
@@ -1,15 +1,25 @@
+
 import { Header } from "../Header";
 import { ArrangementCard } from "../ArrangementCard";
+import { getEvents } from "@/lib/supabase/actionsPublic";
 
-export const ArrangementerPage = () => {
+export const ArrangementerPage = async () => {
+    const { eventPostTypeName, eventData } = await getEvents();
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header
                 title="Arrangementer"
                 description="Her finner du en oversikt over alle kommende arrangementer"
             />
-            <ArrangementCard />
-            <main className="flex-1"></main>
+            <main>
+                <div className="flex gap-6">
+                    <ArrangementCard
+                        eventData={eventData}
+                        eventPostTypeName={eventPostTypeName}
+                    />
+                </div>
+            </main>
         </div>
     );
 };
