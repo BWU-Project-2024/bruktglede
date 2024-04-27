a09b44c40b9a9fa5485d729c0c52c41d482af6b6
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-27 21:41:57 +0200

    fixed article and article id pages

--- components/StoreHeader.jsx
+++ components/StoreHeader.jsx
@@ -1,69 +1,58 @@
 "use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
-import { getStoreVisions } from "@/lib/supabase/actionsPublic";
+import { useState } from "react";
 
-export const StoreHeader = () => {
-    const [storesVision, setStoresVision] = useState([]);
+export const StoreHeader = ({ storeData, storeVisionData }) => {
     const [isVisionVisible, setIsVisionVisible] = useState(false);
 
-    useEffect(() => {
-        const fetchStoreVisions = async () => {
-            const visions = await getStoreVisions();
-            if (visions) {
-                setStoresVision(visions);
-            } else {
-                console.log("No visions available");
-            }
-        };
-
-        fetchStoreVisions();
-    }, []);
+    const OpenCloseVision = () => {
+        setIsVisionVisible(!isVisionVisible)
+    }
 
     return (
-        <>
-            {storesVision.length === 0 ? (
-                <p>No vision available at the moment.</p>
-            ) : (
-                storesVision.map((store, index) => (
-                    <div key={index} className="relative">
-                        <Image
-                            src={store.img}
-                            alt="Store"
-                            layout="responsive"
-                            width={1920}
-                            height={1080}
-                        />
-                        <div
-                            className={`relative inset-x-0 text-center px-7 py-10 bg-ivory-default ${isVisionVisible ? "bg-opacity-90" : "bg-opacity-70"} transition-opacity duration-300`}
-                        >
-                            <div className="absolute lg:-top-44 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[110px] lg:w-[150px] h-[110px] lg:h-[150px] aspect-square rounded-full flex justify-center items-center font-jomhuria text-5xl">
-                                {store.store_name}
-                            </div>
-                            <p className="lg:max-w-2xl mx-auto pt-16">
-                                {store.subtitle}
-                            </p>
-                            {isVisionVisible && (
-                                <p className="text-center mt-5">
-                                    {store.bodyText}
-                                </p>
-                            )}
-                            <div className="flex justify-center pt-5 pb-3">
-                                <button
-                                    onClick={() =>
-                                        setIsVisionVisible(!isVisionVisible)
-                                    }
-                                    className="w-48 bg-ivory-lighter text-forestgreen-default rounded py-1 font-medium drop-shadow hover:bg-forestgreen-default hover:text-ivory-default transition duration-200 text-center"
-                                >
-                                    {isVisionVisible
-                                        ? "Hide Vision"
-                                        : "Show Vision"}
-                                </button>
-                            </div>
-                        </div>
+        <div className="relative">
+            <div className="relative w-full h-[450px]">
+                <Image
+                    src={storeData.img}
+                    alt="Store"
+                    fill
+                    style={
+                        { objectFit: "cover", objectPosition: "center" }
+                    }
+                // layout="fill"
+                // objectFit="cover"
+                // objectPosition="center"
+                // priority
+                />
+            </div>
+            <div className="absolute top-0 left-0 w-full h-[450px] flex flex-col">
+                <div className="h-[50%] flex items-center justify-center">
+                    <div className="w-[110px] lg:w-[150px] h-[110px] lg:h-[150px] aspect-square rounded-full flex justify-center items-center font-jomhuria text-6xl bg-white">
+                        {storeData?.name}
                     </div>
-                ))
+                </div>
+                <div className="h-[50%] flex flex-col items-center justify-center bg-opacity-95 bg-ivory-default gap-16">
+                    <p className="w-[50%] text-wrap flex justify-center">{storeData.description}</p>
+                    <button
+                        onClick={OpenCloseVision}
+                        className="w-48 rounded py-1 font-medium drop-shadow bg-forestgreen-default text-ivory-default hover:bg-ivory-lighter hover:text-forestgreen-default transition duration-200 text-center"
+                    >
+                        {isVisionVisible ? "Lukk butikkens visjon" : "Se butikkens visjon"}
+                    </button>
+                </div>
+            </div>
+            {isVisionVisible && (
+                <div className="text-center">
+                    <h2>{storeVisionData[0].title}</h2>
+                    <h3>{storeVisionData[0].ingress}</h3>
+                    <h4>{storeVisionData[0].subtitle}</h4>
+                    <p>{storeVisionData[0].bodyText}</p>
+                    <Image
+                        src={storeVisionData[0].img}
+                    >
+                    </Image>
+                </div>
             )}
-        </>
+        </div>
     );
 };
