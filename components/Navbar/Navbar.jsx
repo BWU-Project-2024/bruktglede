ef21c092ec0a5b9f8018f86a3d570a6cd22bd22b
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-27 16:37:18 +0200

    fixed session loop

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
 
         fetchSession();
         fetchStores();
-    }, []);
+    }, [userRole]);
 
     const handleNav = () => {
         setMenuOpen((menuOpen) => !menuOpen);
