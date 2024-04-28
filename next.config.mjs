4dc1c00603c96ea22370f33982f064bf4a39a8c3
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-28 11:57:38 +0200

    trying to fix vercel hosting

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,9 @@ const nextConfig = {
     },
     env: {
         NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dmz24qiwu",
-        NEXT_PUBLIC_CLOUDINARY_PRESET_NAME:"gfsyydiz"
+        NEXT_PUBLIC_CLOUDINARY_PRESET_NAME:"gfsyydiz",
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
     images: {
         remotePatterns: [
