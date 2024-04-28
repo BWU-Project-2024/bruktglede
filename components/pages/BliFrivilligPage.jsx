aba8ee3fa9a2d8b66c8b3fa762f1b8a2dcc2a2e3
lisamarimyrene <lisamy@stud.ntnu.no>
2024-04-28 11:33:56 +0200

    UrlPathBar

--- components/pages/BliFrivilligPage.jsx
+++ components/pages/BliFrivilligPage.jsx
@@ -1,27 +1,22 @@
-"use server";
 import { Header } from "../Header"
+import { UrlPath } from "../UrlPath"
 import { BliFrivilligForm } from "../BliFrivilligForm"
 import { H2Section } from "../H2Section"
 import { getStore } from "@/lib/supabase/actionsPublicForms";
 
- 
 export const BliFrivilligPage = async () => {
     const store = await getStore();
     return (
         <div className="flex flex-col min-h-screen">
             <Header title="Vil du bli frivillig?" description="Jobb som frivillig i Gjøvik" />
+            <UrlPath />
             <main className="flex-1  lg:-mt-10 p-2 px-6 md:w-4/5 lg:w-3/4 max-w-readable md:px-20 lg:px-40 lg:pt-20">
-
-            <H2Section heading="Hvorfor bli frivillig?" paragraph="Å jobbe som frivillig i en bruktbutikk er en fantastisk måte å gi tilbake til samfunnet på samtidig som man får verdifull erfaring. Her får du muligheten til å møte nye mennesker og være en del av et positivt fellesskap. Du vil også lære om bærekraftig forbruk og gjenbruk, noe som kan inspirere til mer miljøvennlige vaner." />
-            <H2Section heading="Hva går arbeidet ut på?" paragraph="Arbeidet som frivillig i en bruktbutikk kan variere, men det inkluderer ofte oppgaver som kundeservice, sortering av donerte varer, prising, og organisering av butikkens inventar. Du kan også få muligheten til å delta i markedsføringsaktiviteter, arrangementer og salgskampanjer for å øke butikkens inntekter og synlighet i lokalsamfunnet.
+                <H2Section heading="Hvorfor bli frivillig?" paragraph="Å jobbe som frivillig i en bruktbutikk er en fantastisk måte å gi tilbake til samfunnet på samtidig som man får verdifull erfaring. Her får du muligheten til å møte nye mennesker og være en del av et positivt fellesskap. Du vil også lære om bærekraftig forbruk og gjenbruk, noe som kan inspirere til mer miljøvennlige vaner." />
+                <H2Section heading="Hva går arbeidet ut på?" paragraph="Arbeidet som frivillig i en bruktbutikk kan variere, men det inkluderer ofte oppgaver som kundeservice, sortering av donerte varer, prising, og organisering av butikkens inventar. Du kan også få muligheten til å delta i markedsføringsaktiviteter, arrangementer og salgskampanjer for å øke butikkens inntekter og synlighet i lokalsamfunnet.
 " />
-            <H2Section heading="En til overskrift" paragraph="lala" />
-
-<BliFrivilligForm storeData={store}/>
+                <H2Section heading="En til overskrift" paragraph="lala" />
+                <BliFrivilligForm storeData={store} />
             </main>
-
-
-
         </div>
     )
 }
--- components/pages/HoydepunktPage.jsx
+++ components/pages/HoydepunktPage.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../Header"
+import { UrlPath } from "../UrlPath"
 
 export const HoydepunktPage = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header title="Ukens høydepunkt" description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke." />
+            <UrlPath />
             <main className="flex-1">
 
             </main>
