320f48745efedb11f5c8f234acd7e437474de5d2
lexavan <alexaev@stud.ntnu.no>
2024-04-28 16:37:46 +0200

    Litte style fixes

--- components/ArticleBarCard.jsx
+++ components/ArticleBarCard.jsx
@@ -16,26 +16,30 @@ export const ArticleBarCard = ({ articlesData }) => {
     return (
         <div className="font-opensans">
             {articlesData?.map((article, index) => (
-                <div key={index} className="flex flex-col gap-1">
-                    <Link href={`/artikler/${article.id}`} key={index}>
-                        <span className="text-lg font-medium underline">{article.title}</span>
-                    </Link>
-                    <p className="text-xs mb-1">{formatDate(article.created_at)}</p>
-                    <p className="text-base line-clamp-2">{article.ingress}</p>
-                    <div className="mt-2 mb-5">
-                        <StoreTag storename={article.store_name} className="mr-1" />
-                        {article.tags.map((tag, index) => (
-                            <CategoryTag key={index} title={tag} />
-                        ))}
-                          <Image
-                                    src={article.img}
-                                    alt="artikkel cover bilde"
-                                    width={400}
-                                    height={400}
-                                    className="object-cover hidden md:block w-80 h-80 md:w-64 md:h-64 overflow-hidden "
-                                />
-                        <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                <div key={index} className="flex flex-col">
+                    <div className="flex justify-between">
+                        <div className="flex flex-col gap-1 md:mr-4">
+                            <Link href={`/artikler/${article.id}`} key={index}>
+                                <span className="text-lg font-medium underline">{article.title}</span>
+                            </Link>
+                            <p className="text-xs mb-1">{formatDate(article.created_at)}</p>
+                            <p className="text-base line-clamp-2   ">{article.ingress}</p>
+                            <div className="mt-4 mb-2">
+                                <StoreTag storename={article.store_name} className="mr-1" />
+                                {article.tags.map((tag, index) => (
+                                    <CategoryTag key={index} title={tag} />
+                                ))}
+                            </div>
+                        </div>
+                        <Image
+                            src={article.img}
+                            width={200}
+                            height={100}
+                            alt="Artikkel cover bilde"
+                            className="hidden md:block"
+                        />
                     </div>
+                    <hr className="w-full h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
                 </div>
             ))}
         </div>
--- components/pages/ArtiklerPage.jsx
+++ components/pages/ArtiklerPage.jsx
@@ -10,7 +10,7 @@ export const ArtiklerPage = async () => {
         <div className="flex flex-col min-h-screen">
             <Header title="Siste nytt" description="Her finner du en oversikt over nyheter og artikler fra butikkene" />
             <UrlPath />
-            <main className="flex p-2 px-6 md:px-28 lg:px-64 lg:pt-20">
+            <main className="flex p-2 px-6 md:px-28 lg:px-64 pt-5 lg:pt-10">
                 <ArticleBarCard articlesData={articleInfo.articleData} />
             </main>
         </div>
