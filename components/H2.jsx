b0436addc24239065ff50aa9e801f27059f37f8a
lexavan <alexaev@stud.ntnu.no>
2024-04-30 17:13:25 +0200

    style fixing lala hehe

---
+++ components/H2.jsx
@@ -0,0 +1,12 @@
+export const H2 = ({ heading }) => {
+    return (
+
+        <div className="mb-10">
+        <h2 className="text-lg lg:text-2xl ml-14 -mb-5 font-medium font-opensans ">{heading}</h2>
+       
+        </div>
+
+
+
+    );
+};
--- components/pages/ButikkIdPage.jsx
+++ components/pages/ButikkIdPage.jsx
@@ -3,23 +3,45 @@ import { StoreInfoBar } from "../StoreInfoBar";
 import { StoreHeader } from "../StoreHeader";
 import { UkensHoydepunkt } from "../UkensHoydepunkt";
 import { UrlPath } from "../UrlPath";
+import { ArticleCard } from "../ArticleCard";
+import { ArrangementCard } from "../ArrangementCard";
+import { getTopFourEvents, getTopFourArticles } from "@/lib/supabase/actionsPublic";
+import { H2 } from "../H2";
 
 export const ButikkIdPage = async ({ params }) => {
     const storeData = await getStoreById(params);
     const storeVisionData = await getStoreVisions(params)
+    const [events, articles] = await Promise.all([getTopFourEvents(), getTopFourArticles()]);
+
 
     return (
-        <div className="flex flex-col min-h-screen">
-            <main className="flex-1">
+        <div className="flex flex-col min-h-screen font-opensans">
+           
                 <UrlPath />
                 <StoreHeader
                     storeData={storeData.stores}
                     storeVisionData={storeVisionData}
                 />
+                 <main className="flex-1">
                 <StoreInfoBar
                     storeIdData={storeData}
                 />
+
+
+
+<section className="pt-5 lg:pt-10">
+    <H2 heading="Vi arrangerer"/>
+                <ArticleCard
+                  articleData={articles.articleData}
+                  articlePostTypeName={articles.articlePostType} />
+                </section>
+
+
+<section className="pt-5 lg:pt-10">
+    <H2 heading="Ukens høydepunkt"/>
+               
                 {/* <UkensHoydepunkt /> */}
+                </section>
             </main>
         </div>
     )
