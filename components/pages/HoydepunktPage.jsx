5949bcb8d66f30b971fa008b611fd6ed34af3378
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-30 13:01:11 +0200

    small change

--- components/pages/HoydepunktPage.jsx
+++ components/pages/HoydepunktPage.jsx
@@ -1,15 +1,12 @@
-
 import { Header } from "../Header";
 import { UkensHoydepunkt } from "../UkensHoydepunkt";
 import { getHighlights } from "@/lib/supabase/actionsPublic";
-import { Header } from "../Header"
-import { UrlPath } from "../UrlPath"
+import { UrlPath } from "../UrlPath";
 
 export const HoydepunktPage = async () => {
     const highlightData = await getHighlights();
     return (
         <div className="flex flex-col min-h-screen">
-
             <Header
                 title="Ukens høydepunkt"
                 description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke."
@@ -17,12 +14,12 @@ export const HoydepunktPage = async () => {
             <main className="flex-1"></main>
             <UkensHoydepunkt highlightData={highlightData} />
 
-            <Header title="Ukens høydepunkt" description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke." />
+            <Header
+                title="Ukens høydepunkt"
+                description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke."
+            />
             <UrlPath />
-            <main className="flex-1">
-
-            </main>
-
+            <main className="flex-1"></main>
         </div>
     );
 };
