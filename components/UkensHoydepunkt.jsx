b1977faa2dd76645f104d3bea96eb1838b69a444
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-04-30 11:58:19 +0200

    høydepunkt page

--- components/UkensHoydepunkt.jsx
+++ components/UkensHoydepunkt.jsx
@@ -3,29 +3,24 @@ import Image from "next/image";
 import { StoreTag } from "./StoreTag";
 import React, { useEffect, useState } from "react";
 
-export const UkensHoydepunkt = ({ highlightData = [] }) => {
-    const [highlightsData, setHighlightsData] = useState(highlightData);
+export const UkensHoydepunkt = (highlightData) => {
+    // const [highlightsData, setHighlightsData] = useState([]);
 
-    useEffect(() => {
-        setHighlightsData(highlightData);
-    }, [highlightData]);
-
+    const data = highlightData.highlightData.highlights;
     return (
         <>
-            {highlightsData.length === 0 ? (
-                <p>No highlights available.</p>
+            {highlightData.length === 0 ? (
+                <p>Ingen høydepunkt er tilgjengelig for øyeblikket.</p>
             ) : (
-                highlightsData.map((highlight, index) => (
+                data.map((highlight, index) => (
                     <div key={index} className="relative md:w-2/3 lg:w-5/12">
                         <div className="absolute left-1/2 -translate-x-1/2 bg-forestgreen-default w-11/12 h-11 opacity-70 z-10"></div>
-
                         <div className="absolute left-1/2 -translate-x-1/2 z-20 flex items-center justify-between px-4 pt-2.5 md:pt-1.5 lg:pt-1.5 w-11/12">
                             <p className="text-ivory-default">
                                 {highlight.title}
                             </p>
-                            <StoreTag>{highlight.store_name}</StoreTag>
+                            <StoreTag storename={highlight.store_name} />
                         </div>
-
                         <div className="flex justify-center items-center z-0">
                             <Image
                                 src={highlight.img}
@@ -35,7 +30,6 @@ export const UkensHoydepunkt = ({ highlightData = [] }) => {
                                 className="w-11/12"
                             />
                         </div>
-
                         <div className="mx-auto flex justify-center z-20 bg-ivory-default w-11/12 py-4 px-2 mb-5">
                             <p className="text-black">{highlight.ingress}</p>
                         </div>
--- components/pages/HoydepunktPage.jsx
+++ components/pages/HoydepunktPage.jsx
@@ -1,8 +1,10 @@
 import { Header } from "../Header";
-import { UkensHoydepunkt } from "@/components/UkensHoydepunkt";
-/* import { getWeeklyHighlight } from "@/lib/supabase/actionsPublic"; */
+import { UkensHoydepunkt } from "../UkensHoydepunkt";
+import { getHighlights } from "@/lib/supabase/actionsPublic";
 
-export const HoydepunktPage = () => {
+export const HoydepunktPage = async () => {
+    const highlightData = await getHighlights();
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header
@@ -10,8 +12,7 @@ export const HoydepunktPage = () => {
                 description="Her finner du en oversikt over unike produkter butikkene plukker ut hver uke."
             />
             <main className="flex-1"></main>
-
-            <UkensHoydepunkt />
+            <UkensHoydepunkt highlightData={highlightData} />
         </div>
     );
 };
